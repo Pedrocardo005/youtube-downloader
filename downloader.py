8529fb8ad70c26263621e4d3c5f3ee664c15a223
Pedrocardo005 <pedrocardo007@hotmail.com>
2025-07-09 14:00:21 -0300

    :arrow_up: build: Atualiza dependencia

--- downloader.py
+++ downloader.py
@@ -3,10 +3,18 @@ from pytubefix import YouTube
 url = input("Cole a sua url: ")
 
 try:
-    video = YouTube(url)
-except:
-    print("Ocorreu um erro")
+    video = YouTube(url, use_oauth=True, allow_oauth_cache=True)
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
 
-mp4_streams = video.streams.get_highest_resolution()
+# Verifique se o objeto video foi criado com sucesso antes de prosseguir
+if 'video' in locals() and video:
+    mp4_streams = video.streams.get_highest_resolution()
 
-mp4_streams.download()
+    try:
+        mp4_streams.download()
+        print("Download concluído!")
+    except Exception as e:
+        print(f"Ocorreu um erro durante o download: {e}")
+else:
+    print("Não foi possível processar o vídeo. Verifique a URL.")
